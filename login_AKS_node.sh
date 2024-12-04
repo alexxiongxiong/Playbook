b5d72fcd1c63af74a839bf7fbde891cee32d1020
Alex Xiong <87124423+alexxiongxiong@users.noreply.github.com>
2024-12-04 09:35:24 +0800

    Create login_AKS_node.sh

---
+++ login_AKS_node.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+set -eu
+which kubectl 1>/dev/null || { exit; echo "please install kubectl first"; }
+
+echo "You have the following nodes on your AKS cluster:"
+kubectl get node -o wide;
+
+echo -e "\n";
+
+read -p "please input your node name which you would like to login: " nodeName
+
+# evaluate if the user's input is valid; if no, exit the script
+echo $(kubectl get node -o wide) | grep -w $nodeName 1>/dev/null || { echo "You input an invalid AKS node name. Quit the script"; exit 1; }
+
+# start to deploy previleged pod used to login worker node
+kubectl apply -f - <<EOF
+apiVersion: v1
+kind: Pod
+metadata:
+  name: login-${nodeName}
+  namespace: default
+spec:
+  containers:
+  - command:
+    - nsenter
+    - --mount=/proc/1/ns/mnt
+    - --
+    - su
+    - '-'
+    image: mcr.microsoft.com/devcontainers/base:alpine-3.20
+    imagePullPolicy: IfNotPresent
+    name: alpine
+    resources:
+      requests:
+        cpu: 100m
+        memory: 200Mi
+      limits:
+        cpu: 100m
+        memory: 200Mi
+    securityContext:
+      privileged: true
+    stdin: true
+    terminationMessagePath: /dev/termination-log
+    terminationMessagePolicy: File
+    tty: true
+  enableServiceLinks: true
+  hostNetwork: true
+  hostPID: true
+  nodeSelector:
+    kubernetes.io/hostname: ${nodeName}
+EOF
+
+# evaluate if the pod deployment is successful and wait for pods coming up
+[ $? -eq 0 ] && echo -e "\nThe Pod login-${nodeName} is coming up! please wait for 10 seconds";
+sleep 10;
+
+# guide user to login AKS node via new-created pod
+echo -e "\nThe Pod login-${nodeName} has been created in your default namespace. \n\nplease run \"kubectl exec -ti login-${nodeName} -n default bash\" to login worker node\n";
